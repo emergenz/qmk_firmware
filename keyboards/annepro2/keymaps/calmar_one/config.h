3851a3a100e8139439aaca1586040e167505290e
Franz Srambical <franz.srambical@gmail.com>
2025-09-24 09:01:30 +0200

    feat: miryoku-like bindings for anne pro

---
+++ keyboards/annepro2/keymaps/calmar_one/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+
+#define MOUSEKEY_DELAY 5
+#define MOUSEKEY_INTERVAL 8
+#define MOUSEKEY_MOVE_DELTA 4
+#define MOUSEKEY_MAX_SPEED 16
+#define MOUSEKEY_TIME_TO_MAX 20
+
+// Wheel left at defaults; adjust after testing if needed
